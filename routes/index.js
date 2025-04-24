69b06ffd8a050947df69099149b474af902cf496
Valeriy Svydenko <vsvydenk@redhat.com>
2025-04-24 09:03:35 +0000

    chore:update sample
    Signed-off-by: Valeriy Svydenko <vsvydenk@redhat.com>

--- routes/index.js
+++ routes/index.js
@@ -3,33 +3,40 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-const Message = require('./messages')
+const Message = require('./messages');
 
 const getMessages = async () => {
-    let list = []
-    await Message.messageModel.find({}, null, { sort: { '_id': -1 } }, (err, messages) => {
+    let list = [];
+    try {
+        // Find messages sorted by _id in descending order
+        const messages = await Message.messageModel.find({}).sort({ '_id': -1 });
+        
+        // Process messages if they exist
         if (messages.length > 0) {
             messages.forEach((message) => {
                 if (message.name && message.body) {
-                    list.push({ 'name': message.name, 'body': message.body, 'timestamp': message._id.getTimestamp() })
+                    list.push({ 'name': message.name, 'body': message.body, 'timestamp': message._id.getTimestamp() });
                 }
             });
         }
-    });
-    return list;
-}
-
-const setMessage = (name, message) => {
-    try {
-        Message.create(({name: name, body: message}))
     } catch (err) {
-        if (err.name == "ValidationError") {
-            console.error('validation error: ' + err)
+        console.error('Error retrieving messages:', err);
+    }
+    return list;
+};
+
+const setMessage = async (name, message) => {
+    try {
+        // Create new message and save it
+        await Message.create({ name: name, body: message });
+    } catch (err) {
+        if (err.name === 'ValidationError') {
+            console.error('Validation error: ' + err);
         } else {
-            console.error('could not save: ' + err)
+            console.error('Could not save message: ' + err);
         }
     }
-}
+};
 
 module.exports = {
     getMessages,
